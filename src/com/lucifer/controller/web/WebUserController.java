54c84e1a4f2fb977264815bbd434e5bb052aed2e
刘峰选 <liufengxuan@letv.com>
2015-08-04 17:59:06 +0800

    test

--- src/com/lucifer/controller/web/WebUserController.java
+++ src/com/lucifer/controller/web/WebUserController.java
@@ -1,8 +1,13 @@
 package com.lucifer.controller.web;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.lucifer.model.User;
+import com.lucifer.util.ViewHelper;
 
 @Controller
 public class WebUserController {
@@ -15,8 +20,14 @@ public class WebUserController {
 	
 	
 	@RequestMapping(value = "/manage/info_set", method = RequestMethod.GET)
-	public String infoSet(){
+	public String infoSet(HttpServletRequest request){
+		User user = ViewHelper.getInstance().getWebTokenUser(request);
 		return "/WEB-INF/web/manage/user/info_set.jsp";
+	}
+	
+	@RequestMapping(value = "/manage/info_set", method = RequestMethod.POST)
+	public String infoSetSubmit(){
+		return "redirect:/manage/info_set";
 	}
 
 }
