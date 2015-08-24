92d43ec76ac516e7881ed171892af6046a50702a
刘峰选 <liufengxuan@letv.com>
2015-08-24 17:19:32 +0800

    resume

---
+++ src/com/lucifer/model/ResumeExpand.java
@@ -0,0 +1,46 @@
+package com.lucifer.model;
+
+public class ResumeExpand {
+
+	private Long id;
+
+	private Long resume_id;
+	
+	private String name;
+	
+	private String description;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getResume_id() {
+		return resume_id;
+	}
+
+	public void setResume_id(Long resume_id) {
+		this.resume_id = resume_id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+}
---
+++ src/com/lucifer/model/Train.java
@@ -0,0 +1,78 @@
+package com.lucifer.model;
+
+import java.util.Date;
+
+public class Train {
+	
+	private Long id;
+
+	private Long resume_id;
+
+	private Date start_date;
+
+	private Date end_date;
+	
+	private String organization;
+	
+	private String course;
+	
+	private String description;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getResume_id() {
+		return resume_id;
+	}
+
+	public void setResume_id(Long resume_id) {
+		this.resume_id = resume_id;
+	}
+
+	public Date getStart_date() {
+		return start_date;
+	}
+
+	public void setStart_date(Date start_date) {
+		this.start_date = start_date;
+	}
+
+	public Date getEnd_date() {
+		return end_date;
+	}
+
+	public void setEnd_date(Date end_date) {
+		this.end_date = end_date;
+	}
+
+	public String getOrganization() {
+		return organization;
+	}
+
+	public void setOrganization(String organization) {
+		this.organization = organization;
+	}
+
+	public String getCourse() {
+		return course;
+	}
+
+	public void setCourse(String course) {
+		this.course = course;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+}
