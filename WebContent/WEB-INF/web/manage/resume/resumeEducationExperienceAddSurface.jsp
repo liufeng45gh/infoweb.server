a7301d53db9aeac8876df09d8c8f78bbcd509458
刘峰选 <liufengxuan@letv.com>
2015-08-25 15:10:49 +0800

    resume

---
+++ WebContent/WEB-INF/web/manage/resume/resumeEducationExperienceAddSurface.jsp
@@ -0,0 +1,11 @@
+<%@ page language="java" pageEncoding="utf-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt" %>
+
+<div  class="center" style="width:900px;">
+<form action="/manage/resume/train/add" method="post">
+<jsp:include page="resumeEducationExperienceFiled.jsp"></jsp:include>
+</form>
+
+</div>
+
--- WebContent/script/web/resume.js
+++ WebContent/script/web/resume.js
@@ -775,6 +775,86 @@ function modifyProjectExperience(id){
 	});
 }
 
+
+
+function addEducationExperienceInput(id){
+	var send_data={};
+	   //alert(1);
+	send_data.random = Math.random();
+	send_data.resume_id = id;
+	var account_request =$.ajax({
+	   type: 'get',
+	   url: '/manage/resume/education_experience/add',
+	   data: send_data,
+	   dataType: 'html'
+	});
+
+	account_request.fail(function( jqXHR, textStatus ) {
+	  if(jqXHR.status==401){
+	     //openWeiboLogin();
+		  
+	  }
+	});
+	
+	account_request.done(function(data) {
+		$("#modify_inner_div").css("display","block");
+		$("#modify_inner_div").html(data); 
+	});
+}
+
+
+function deleteEducationExperience(id){
+	if(!confirm("确认删除教育经历?")){
+		return ;
+	}
+	var send_data={};
+	   //alert(1);
+	send_data.random = Math.random();
+	send_data.id = id;
+	var account_request =$.ajax({
+	   type: 'post',
+	   url: '/manage/resume/education_experience/delete',
+	   data: send_data,
+	   dataType: 'json'
+	});
+
+	account_request.fail(function( jqXHR, textStatus ) {
+	  if(jqXHR.status==401){
+	     //openWeiboLogin();
+		  
+	  }
+	});
+	
+	account_request.done(function(data) {
+		window.location.reload();
+	});
+}
+
+function modifyEducationExperience(id){
+	var send_data={};
+	   //alert(1);
+	send_data.random = Math.random();
+	send_data.id = id;
+	var account_request =$.ajax({
+	   type: 'get',
+	   url: '/manage/resume/education_experience/update',
+	   data: send_data,
+	   dataType: 'html'
+	});
+
+	account_request.fail(function( jqXHR, textStatus ) {
+	  if(jqXHR.status==401){
+	     //openWeiboLogin();		  
+	  }
+	});
+	
+	account_request.done(function(data) {
+		$("#modify_inner_div").css("display","block");
+		$("#modify_inner_div").html(data); 
+	});
+}
+
+
 function addEduInput(id){
 	var send_data={};
 	   //alert(1);
